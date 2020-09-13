9d4628699b47af21ae5e196235e71549e57948a4
Felipe Oliveira <felipe2.oliveira@gmail.com>
2020-09-13 18:55:24 -0300

    validação do form sign e sign up e estrutura do toast

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,8 +6,10 @@ import React, {
   useCallback,
 } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
-import { Container } from './styles';
+
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -21,6 +23,10 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   const [isField, setIsFilled] = useState(false);
 
   const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
 
   // funcao utilizando callback para a função nao ser recriada na memoria toda vez que esse componente for renderizado
   const handleInputBlur = useCallback(() => {
@@ -39,15 +45,21 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isFilled={isField} isFocused={isFocused}>
+    <Container isErrored={!!error} isFilled={isField} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
-        onFocus={() => setIsFocused(true)}
+        onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
         {...rest}
       />
+
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
     </Container>
   );
 };
