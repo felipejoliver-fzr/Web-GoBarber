21179a652624185434e3527622771b60e8a20db9
Felipe Oliveira <felipe2.oliveira@gmail.com>
2020-09-14 20:16:54 -0300

    rotas e autenticação

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signInBackgroundImg from '../../assets/sign-in-background.png';
@@ -15,12 +15,30 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  /* place-content pega todo o conteudo que tem nessa div no caso content e insere tudo ao centro idenpendente se é eixo horizontal ou vertical */
-  place-content: center;
+  justify-content: center;
 
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
